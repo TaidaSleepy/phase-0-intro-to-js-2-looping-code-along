5c8a1f3874ac95a37ea16851d475d368774be323
Taida <samuelgbusinessacc@gmail.com>
2025-08-31 11:04:26 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    let count = number;
+    while (count >= 0) {
+        console.log(count);
+        count--;
+    } 
+}      
